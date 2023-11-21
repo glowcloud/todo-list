64d035adcd669864ce536f6c15f8b9e5175013c1
Oliwia Rogala <44094416+glowcloud@users.noreply.github.com>
2023-11-21 12:23:41 +0100

    Changed modal border color

--- frontend/src/components/shared/CustomModal.jsx
+++ frontend/src/components/shared/CustomModal.jsx
@@ -10,7 +10,7 @@ const CustomModal = ({ children, isOpen, handleClose }) => {
           left: "50%",
           transform: "translate(-50%, -50%)",
           bgcolor: "background.paper",
-          border: "2px solid #000",
+          border: "2px solid #aaa",
           width: { xs: 350, sm: 550, md: 700, lg: 800 },
           px: 5,
           pb: 5,
