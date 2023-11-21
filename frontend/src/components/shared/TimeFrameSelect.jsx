5e0a5640da1a0a81554a06f6f3b267b685e06a7f
Oliwia Rogala <44094416+glowcloud@users.noreply.github.com>
2023-11-21 13:43:58 +0100

    Added start and end week date for time picker

--- frontend/src/components/shared/TimeFrameSelect.jsx
+++ frontend/src/components/shared/TimeFrameSelect.jsx
@@ -1,5 +1,6 @@
 import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
 
 const TimeFrameSelect = ({
   timeFrame,
@@ -9,15 +10,20 @@ const TimeFrameSelect = ({
 }) => {
   const handleTimeFrameChange = (e) => {
     setTimeFrame(e.target.value);
+    setChosenTime(dayjs());
   };
 
   const handleChosenTimeChange = (value) => {
-    setChosenTime(value);
+    if (timeFrame === "week") {
+      setChosenTime(dayjs(value).startOf("w"));
+    } else {
+      setChosenTime(value);
+    }
   };
 
   return (
     <Box textAlign="center">
-      <FormControl sx={{ width: 150, mx: 2, my: {xs: 1, sm: 0} }}>
+      <FormControl sx={{ width: 150, mx: 2, my: 1 }}>
         <InputLabel>Timeframe</InputLabel>
         <Select value={timeFrame} onChange={handleTimeFrameChange}>
           <MenuItem value="overall">Overall</MenuItem>
@@ -28,19 +34,44 @@ const TimeFrameSelect = ({
         </Select>
       </FormControl>
       {timeFrame !== "overall" && (
-        <DatePicker
-          label="Time"
-          views={
-            timeFrame === "year"
-              ? ["year"]
-              : timeFrame === "month"
-              ? ["month", "year"]
-              : ["day", "month", "year"]
-          }
-          value={chosenTime}
-          onChange={handleChosenTimeChange}
-          sx={{ my: { xs: 1, sm: 0 } }}
-        />
+        <Box>
+          <DatePicker
+            label={
+              timeFrame === "week"
+                ? "Start"
+                : timeFrame === "year"
+                ? "Year"
+                : timeFrame === "month"
+                ? "Month"
+                : "Day"
+            }
+            views={
+              timeFrame === "year"
+                ? ["year"]
+                : timeFrame === "month"
+                ? ["month", "year"]
+                : ["day", "month", "year"]
+            }
+            openTo={
+              timeFrame === "year"
+                ? "year"
+                : timeFrame === "month"
+                ? "month"
+                : "day"
+            }
+            value={chosenTime}
+            onChange={handleChosenTimeChange}
+            sx={{ mt: { xs: 2, sm: 1 }, mx: 1 }}
+          />
+          {timeFrame === "week" && (
+            <DatePicker
+              label="End"
+              value={dayjs(chosenTime).endOf("w")}
+              sx={{ mt: { xs: 2, sm: 1 } }}
+              onChange={handleChosenTimeChange}
+            />
+          )}
+        </Box>
       )}
     </Box>
   );
